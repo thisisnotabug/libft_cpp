2a7ef8feaa3e4a39217ac3d3b2378d11739edb3b
Ngoguey42 <ngoguey@student.42.fr>
2016-04-09 15:31:03 +0200

    "improved" padformat

--- ft/padformat.cpp
+++ ft/padformat.cpp
@@ -6,33 +6,38 @@
 //   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2015/11/10 14:57:32 by ngoguey           #+#    #+#             //
-//   Updated: 2015/11/23 09:26:10 by ngoguey          ###   ########.fr       //
+//   Updated: 2016/04/09 15:22:03 by ngoguey          ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
 #include "ft/assert.hpp"
 #include "ft/utils.hpp"
-#include <vector>
+#include <stack>
+#include <array>
 #include <tuple>
 #include <iostream>
+#include <vector>
+#include <algorithm>
+// #include <iostream>
 
-namespace ft
+// v2 of this file
+
+namespace ft // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+{ // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+namespace detail // ~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+{ // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+
+
+using lineinfo_t = std::tuple<int, std::string, std::string>;
+static std::vector<lineinfo_t>  padstack{};
+static std::array<std::string, 6> const colors{
+	"\033[42m", "\033[43m", "\033[44m", "\033[45m", "\033[46m", "\033[47m",
+		};
+
+std::string truncmid(std::string const &str, int w /*= 11*/)
 {
-
-std::vector<std::tuple<int, std::string, std::string>>	padstack{};
-std::vector<std::string> const							colors{
-	"\033[42m",
-	"\033[43m",
-	"\033[44m",
-	"\033[45m",
-	"\033[46m",
-	"\033[47m",
-};
-
-std::string		truncmid(std::string const &str, int w /* = 11*/)
-{
-	int const	sz = str.size();
-	int			right;
+	int const sz = str.size();
+	int right;
 
 	if (sz <= w)
 	{
@@ -45,31 +50,30 @@ std::string		truncmid(std::string const &str, int w /* = 11*/)
 		+ str.substr(str.size() - right, right);
 }
 
-static void		truncate_mid(std::string &str)
+static void truncate_mid(std::string &str)
 {
-	str = str.substr(0, 5) + "." + str.substr(str.size() - 5, 5);
+	str = truncmid(str, 13);
+	// str = str.substr(0, 7) + "." + str.substr(str.size() - 7, 7);
 	return ;
 }
 
-static void		applywidth(
+static void applywidth(
 	std::string &str, unsigned int curwidth, unsigned int width)
 {
 	if (curwidth < width)
-	{
-		str += std::string(width - curwidth, ' ');
-	}
+		str += std::string(std::max(0u, width - curwidth), ' ');
 	return ;
 }
 
-static std::string	build_header(void)
+static std::string  build_header(void)
 {
-	std::string		str("");
-	unsigned int	i;
-	int				line;
-	std::string		file;
-	std::string		fun;
-	std::size_t		pos;
-	std::string		linestr;
+	std::string     str("");
+	unsigned int    i;
+	int             line;
+	std::string     file;
+	std::string     fun;
+	std::size_t     pos;
+	std::string     linestr;
 
 	for (i = 0; i < padstack.size(); i++)
 		str += colors[i % colors.size()] + "  ";
@@ -79,12 +83,10 @@ static std::string	build_header(void)
 	std::tie(line, file, fun) = padstack.back();
 	pos = file.find_last_of('/');
 	file = file.substr(pos + 1, file.find_last_of('.') - pos - 1);
-	if (file.size() > 10)
-		truncate_mid(file);
+	truncate_mid(file);
 	applywidth(file, file.size(), 11);
 
-	if (fun.size() > 10)
-		truncate_mid(fun);
+	truncate_mid(fun);
 	applywidth(fun, fun.size(), 11);
 
 	linestr = ft::f("%", line);
@@ -98,16 +100,18 @@ static std::string	build_header(void)
 std::string padformat_openlvl(
 	int line, std::string const &file, std::string const &function)
 {
-	std::string		str;
-
 	padstack.emplace_back(line, file, function);
-	str = build_header();
-	return str;
+	return build_header();
 }
 
-void		padformat_closelvl(void)
+void padformat_closelvl(void)
 {
-	padstack.pop_back();
+	if (padstack.size() == 0)
+	{
+		std::cout << "EXTRANEOUS FTPADE()" << std::endl;
+		return ;
+	}
+	(void)padstack.pop_back();
 	if (padstack.size() == 0)
 		std::cout << "...done" << std::endl;
 	return ;
@@ -116,29 +120,33 @@ void		padformat_closelvl(void)
 std::string padformat_singlelvl(
 	int line, std::string const &file, std::string const &function)
 {
-	std::string		str;
-	int				lineback;
-	std::string		fileback;
-	std::string		funback;
+	std::string str;
+	lineinfo_t backLine;
 
-	std::tie(lineback, fileback, funback) = padstack.back();
-	if (file == fileback && function == funback && line > lineback)
+	if (padstack.size() > 0)
 	{
-		auto const save	= padstack.back();
-
-		padstack.pop_back();
-		padstack.emplace_back(line, file, function);
-		str = build_header();
-		padstack.pop_back();
-		padstack.push_back(save);
+		backLine = padstack.back();
+		if (file == std::get<1>(backLine)
+			&& function == std::get<2>(backLine)
+			&& line >= std::get<0>(backLine))
+		{
+			padstack.pop_back();
+			padstack.emplace_back(line, file, function);
+			str = build_header();
+			padstack.pop_back();
+			padstack.push_back(backLine);
+			return str;
+		}
 	}
-	else
-	{
-		padstack.emplace_back(line, file, function);
-		str = build_header();
-		padstack.pop_back();
-	}
+	padstack.emplace_back(line, file, function);
+	str = build_header();
+	padstack.pop_back();
 	return str;
 }
 
-};
+
+}; // ~~~~~~~~~~~~~~~~~ END OF NAMESPACE DETAIL //
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+
+}; // ~~~~~~~~~~~~~~~~~~~~~ END OF NAMESPACE FT //
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
--- ft/public/padformat.hpp
+++ ft/public/padformat.hpp
@@ -6,7 +6,7 @@
 //   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2015/12/07 15:41:51 by jaguillo          #+#    #+#             //
-//   Updated: 2015/12/07 15:44:36 by jaguillo         ###   ########.fr       //
+//   Updated: 2016/04/09 15:29:42 by ngoguey          ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
@@ -24,14 +24,20 @@ namespace ft
 ** FT::pad printing
 */
 
-std::string		truncmid(std::string const &str, int w = 11);
+std::string truncmid(std::string const &str, int w = 11);
+
+
+namespace detail // ~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+{ // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+
 
 std::string	padformat_openlvl(
 	int line, std::string const &file, std::string const &function);
-void		padformat_closelvl();
+void padformat_closelvl();
 std::string	padformat_singlelvl(
 	int line, std::string const &file, std::string const &function);
 
+// FT PAD Begin, without details
 inline void	padformat_begin(
 	int line, std::string const &file, std::string const &function)
 {
@@ -41,6 +47,7 @@ inline void	padformat_begin(
 	return ;
 }
 
+// FT PAD Begin, with details
 template <typename ...ARGS>
 void		padformat_begin(
 	int line, std::string const &file, std::string const &function,
@@ -52,41 +59,72 @@ void		padformat_begin(
 	return ;
 }
 
+// FT PAD End
 inline void	padformat_end(void)
 {
 	padformat_closelvl();
 	return ;
 }
 
+// FT PAD single, without details
 inline void	padformat_single(
 	int line, std::string const &file, std::string const &function)
 {
-	std::string const	header = padformat_singlelvl(line, file, function);
+	std::string const header = padformat_singlelvl(line, file, function);
 
 	std::cout << header << '\n';
 	return ;
 }
 
+// FT PAD single, with details
 template <typename ...ARGS>
 void		padformat_single(
 	int line, std::string const &file, std::string const &function,
-	const char *fmt , ARGS ...args)
+	const char *fmt, ARGS ...args)
 {
-	std::string const	header = padformat_singlelvl(line, file, function);
+	std::string const header = padformat_singlelvl(line, file, function);
 
 	std::cout << header << "-> " << ft::f(fmt, args...) << '\n';
 	return ;
 }
 
-# define FTPADE() ft::padformat_end()
+class PadScope
+{
+	int _line;
+	std::string _file, _function;
 
+public:
+	inline PadScope(int line, char const *file, char const *function,
+					std::string &&info) :
+		_line(line), _file(file), _function(function) {
+
+		ft::detail::padformat_begin(line, file, function, "Begin %", info);
+	}
+	inline ~PadScope() {
+
+		ft::detail::padformat_single(_line, _file, _function, "End");
+		ft::detail::padformat_end();
+	}
+};
+
+}; // ~~~~~~~~~~~~~~~~~ END OF NAMESPACE DETAIL //
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+
+# define FTPADE() ft::detail::padformat_end()
 #  ifdef __clang__
-# define FTPADB(...) ft::padformat_begin(__LINE__, __FILE__, __FUNCTION__, ##__VA_ARGS__)
-# define FTPAD(...) ft::padformat_single(__LINE__, __FILE__, __FUNCTION__, ##__VA_ARGS__)
+# define FTPADB(...) ft::detail::padformat_begin(			\
+		__LINE__, __FILE__, __FUNCTION__, ##__VA_ARGS__)
+# define FTPAD(...) ft::detail::padformat_single(			\
+		__LINE__, __FILE__, __FUNCTION__, ##__VA_ARGS__)
+# define FTPADSCOPE(...) ft::detail::PadScope __PADSCOPE{		\
+		__LINE__, __FILE__, __FUNCTION__, ft::f(__VA_ARGS__)}
+
 #  else
 // Small fix
-# define FTPADB(...) ft::padformat_begin(__LINE__, __FILE__, __FUNCTION__, "%", ft::f(__VA_ARGS__))
-# define FTPAD(...) ft::padformat_single(__LINE__, __FILE__, __FUNCTION__, "%", ft::f(__VA_ARGS__))
+# define FTPADB(...) ft::detail::padformat_begin(					\
+		__LINE__, __FILE__, __FUNCTION__, "%", ft::f(__VA_ARGS__))
+# define FTPAD(...) ft::detail::padformat_single(					\
+		__LINE__, __FILE__, __FUNCTION__, "%", ft::f(__VA_ARGS__))
 #  endif
 
 };
